3fc1fb6c5e6e5e17f721aa465cad8c1fd0fd0b60
Sebas-Armas <43274014+Sebas-Armas@users.noreply.github.com>
2023-02-12 14:22:23 -0500

    Change Dev BD to MySQL

--- src/main/webapp/app/config/language.constants.ts
+++ src/main/webapp/app/config/language.constants.ts
@@ -4,8 +4,5 @@
 */
 export const LANGUAGES: string[] = [
   'es',
-  'en',
-  'fr',
-  'de',
   // jhipster-needle-i18n-language-constant - JHipster will add/remove languages in this array
 ];
--- src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -3,9 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'findLanguageFromKey' })
 export class FindLanguageFromKeyPipe implements PipeTransform {
   private languages: { [key: string]: { name: string; rtl?: boolean } } = {
-    en: { name: 'English' },
-    fr: { name: 'Français' },
-    de: { name: 'Deutsch' },
     es: { name: 'Español' },
     // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
   };
